e19963bb6267f2033b6ef0cf0d25e4ca8e97b4a8
Josue Pena <josue.pena.business@gmail.com>
2025-02-09 17:06:08 +0000

    everything but the graph

--- umbrella.rb
+++ umbrella.rb
@@ -5,7 +5,7 @@ require "dotenv/load"
 puts "Welcome to Umbrella! Here, we'll try to help you stay dry out there."
 puts "Where are you? As in, in the world, specifically. Don't tell me the room you're in, just your approximate adress."
 
-user_location = "chicago"
+user_location = gets.chomp
 
 maps_url = "https://maps.googleapis.com/maps/api/geocode/json?address=" + user_location + "&key=" + ENV.fetch("GMAPS_KEY")
 
@@ -38,18 +38,37 @@ minutely = raw_weather.fetch("minutely")
 
 minutely_sum = minutely.fetch("summary") 
 
+currently_weather = raw_weather.fetch("currently")
+
+present_time = currently_weather.fetch("time")
+
 present_sum = "The weather will be " + minutely_sum.downcase + " for the next hour."
 
 present_temp = "The currect temperature is " + hourly_data.at(0).fetch("temperature").to_s + " degrees."
 
 present_report = present_temp + present_sum
 
-pp present_report
-
 rain = hourly_data.find do |hour|
-  hour.fetch("precipProbability") >= 0.15
-  end
+hour.fetch("precipProbability") >= 0.1
+end
 
-  pp rain 
-    
- 
+if rain == nil
+    puts "Sorry, no time of precipitation available for that location."
+else
+  time_at_rain = rain.fetch("time")
+
+  time_to_rain = (time_at_rain - present_time) / 3600
+  
+  puts present_report
+
+  if time_to_rain > 12
+    puts "You probably won't need an umbrella today."
+  else 
+    if time_to_rain < 0
+    puts "It might be raining currently."
+    puts "You might want to carry an umbrella!"
+    else puts "It might rain in " + time_to_rain.to_s + " hours."
+    puts "You might want to carry an umbrella!"
+    end
+  end
+end
